710bdd6746b2684776402462a74b0859b4da0195
ezekiel-g <40302800+ezekiel-g@users.noreply.github.com>
2025-04-26 15:43:41 -0400

    Added gift routes

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,16 +1,16 @@
 router.get('/', async (request, response) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
+        const db = await connectToDatabase()
 
         // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+        const collection = db.collection('gifts')
 
         // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        // const gifts = {{insert code here}}
+        const gifts = await collection.find({}).toArray()
 
         // Task 4: return the gifts using the response.json method
-        response.json(/* {{insert code here}} */)
+        response.status(200).json(gifts)
     } catch (error) {
         console.error('Error fetching gifts:', error)
         response.status(500).send('Error fetching gifts')
@@ -20,21 +20,21 @@ router.get('/', async (request, response) => {
 router.get('/:id', async (request, response) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
+        const db = await connectToDatabase()
 
         // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+        const collection = db.collection('gifts')
 
         const id = request.params.id
 
         // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-        // {{insert code here}}
+        const gift = await collection.findOne({ id: id })
 
         if (!gift) {
             return response.status(404).send('Gift not found')
         }
 
-        response.json(gift)
+        response.status(200).json(gift)
     } catch (error) {
         console.error('Error fetching gift:', error)
         response.status(500).send('Error fetching gift')
