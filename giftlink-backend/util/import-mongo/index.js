41aad0eaeaa08c7cf0cb79ce436e98e361c4d8fa
ezekiel-g <40302800+ezekiel-g@users.noreply.github.com>
2025-04-26 16:54:45 -0400

    Added sentiment analysis

--- giftlink-backend/util/import-mongo/index.js
+++ giftlink-backend/util/import-mongo/index.js
@@ -1,50 +1,50 @@
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
-const fs = require('fs');
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient
+const fs = require('fs')
 
 // MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`;
-let filename = `${__dirname}/gifts.json`;
-const dbName = 'giftdb';
-const collectionName = 'gifts';
+let url = `${process.env.MONGO_URL}`
+let filename = `${__dirname}/gifts.json`
+const dbName = 'giftdb'
+const collectionName = 'gifts'
 
 // notice you have to load the array of gifts into the data object
-const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs;
+const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs
 
 // connect to database and insert data into the collection
 async function loadData() {
-    const client = new MongoClient(url);
+    const client = new MongoClient(url)
 
     try {
         // Connect to the MongoDB client
-        await client.connect();
-        console.log("Connected successfully to server");
+        await client.connect()
+        console.log('Connected successfully to server')
 
         // database will be created if it does not exist
-        const db = client.db(dbName);
+        const db = client.db(dbName)
 
         // collection will be created if it does not exist
-        const collection = db.collection(collectionName);
-        let cursor = await collection.find({});
-        let documents = await cursor.toArray();
+        const collection = db.collection(collectionName)
+        let cursor = await collection.find({})
+        let documents = await cursor.toArray()
 
-        if(documents.length == 0) {
+        if (documents.length == 0) {
             // Insert data into the collection
-            const insertResult = await collection.insertMany(data);
-            console.log('Inserted documents:', insertResult.insertedCount);
+            const insertResult = await collection.insertMany(data)
+            console.log('Inserted documents:', insertResult.insertedCount)
         } else {
-            console.log("Gifts already exists in DB")
+            console.log('Gifts already exists in DB')
         }
-    } catch (err) {
-        console.error(err);
+    } catch (error) {
+        console.error(error.message)
     } finally {
         // Close the connection
-        await client.close();
+        await client.close()
     }
 }
 
-loadData();
+loadData()
 
 module.exports = {
-    loadData,
-  };
+    loadData
+}
