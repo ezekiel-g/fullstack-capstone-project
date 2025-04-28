4f79df4f077f6a02b4debd50914c0501715bab84
ezekiel-g <40302800+ezekiel-g@users.noreply.github.com>
2025-04-28 04:19:25 -0400

    Semicolons

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,25 +1,25 @@
 /*jshint esversion: 8 */
-const express = require('express');
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const connectToDatabase = require('../models/db');
-const router = express.Router();
-const dotenv = require('dotenv');
-const pino = require('pino');
-const { body, validationResult } = require('express-validator');
+const express = require('express')
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const connectToDatabase = require('../models/db')
+const router = express.Router()
+const dotenv = require('dotenv')
+const pino = require('pino')
+const { body, validationResult } = require('express-validator')
 
 dotenv.config()
 
 // Create a Pino logger instance
 const logger = pino()
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET
 
 router.post('/register', async (request, response) => {
     try {
         //Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
-        const db = await connectToDatabase();
-        const collection = db.collection('users');
+        const db = await connectToDatabase()
+        const collection = db.collection('users')
         const existingEmail = await collection.findOne({
             email: request.body.email
         })
@@ -29,9 +29,9 @@ router.post('/register', async (request, response) => {
             return response.status(400).json({ error: 'Email id already exists' })
         }
 
-        const salt = await bcryptjs.genSalt(10);
-        const hash = await bcryptjs.hash(request.body.password, salt);
-        const email = request.body.email;
+        const salt = await bcryptjs.genSalt(10)
+        const hash = await bcryptjs.hash(request.body.password, salt)
+        const email = request.body.email
         console.log('email is', email)
         const newUser = await collection.insertOne({
             email: request.body.email,
@@ -41,9 +41,9 @@ router.post('/register', async (request, response) => {
             createdAt: new Date()
         })
 
-        const payload = { user: { id: newUser.insertedId } };
+        const payload = { user: { id: newUser.insertedId } }
 
-        const authtoken = jwt.sign(payload, JWT_SECRET);
+        const authtoken = jwt.sign(payload, JWT_SECRET)
         logger.info('User registered successfully')
         response.json({ authtoken, email })
     } catch (error) {
@@ -54,25 +54,25 @@ router.post('/register', async (request, response) => {
 
 router.post('/login', async (request, response) => {
     try {
-        const db = await connectToDatabase();
-        const collection = db.collection('users');
-        const theUser = await collection.findOne({ email: request.body.email });
+        const db = await connectToDatabase()
+        const collection = db.collection('users')
+        const theUser = await collection.findOne({ email: request.body.email })
 
         if (theUser) {
             let result = await bcryptjs.compare(
                 request.body.password,
                 theUser.password
-            );
+            )
 
             if (!result) {
                 logger.error('Passwords do not match')
                 return response.status(404).json({ error: 'Wrong pasword' })
             }
 
-            let payload = { user: { id: theUser._id.toString() } };
-            const userName = theUser.firstName;
-            const userEmail = theUser.email;
-            const authtoken = jwt.sign(payload, JWT_SECRET);
+            let payload = { user: { id: theUser._id.toString() } }
+            const userName = theUser.firstName
+            const userEmail = theUser.email
+            const authtoken = jwt.sign(payload, JWT_SECRET)
 
             logger.info('User logged in successfully')
             return response.status(200).json({ authtoken, userName, userEmail })
@@ -86,7 +86,7 @@ router.post('/login', async (request, response) => {
             .status(500)
             .json({ error: 'Internal server error', details: error.message })
     }
-});
+})
 
 // Update user
 router.put('/update', async (request, response) => {
@@ -98,7 +98,7 @@ router.put('/update', async (request, response) => {
     }
 
     try {
-        const email = request.headers.email;
+        const email = request.headers.email
 
         if (!email) {
             logger.error('Email not found in the request headers')
@@ -107,9 +107,9 @@ router.put('/update', async (request, response) => {
                 .json({ error: 'Email not found in the request headers' })
         }
 
-        const db = await connectToDatabase();
-        const collection = db.collection('users');
-        const existingUser = await collection.findOne({ email });
+        const db = await connectToDatabase()
+        const collection = db.collection('users')
+        const existingUser = await collection.findOne({ email })
 
         if (!existingUser) {
             logger.error('User not found')
@@ -123,10 +123,10 @@ router.put('/update', async (request, response) => {
             { email },
             { $set: existingUser },
             { returnDocument: 'after' }
-        );
+        )
 
-        const payload = { user: { id: updatedUser._id.toString() } };
-        const authtoken = jwt.sign(payload, JWT_SECRET);
+        const payload = { user: { id: updatedUser._id.toString() } }
+        const authtoken = jwt.sign(payload, JWT_SECRET)
 
         logger.info('User updated successfully')
         response.json({ authtoken })
@@ -136,4 +136,4 @@ router.put('/update', async (request, response) => {
     }
 })
 
-module.exports = router;
+module.exports = router
